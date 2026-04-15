1f48cab07652d4252352cf41c462c05ba0121be8
Alexander Sinisterra <alexander.sinisterra.moreno@gmail.com>
2026-04-15 11:48:23 -0500

    Comentarios añadidos

--- Backend/main.py
+++ Backend/main.py
@@ -3,6 +3,19 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/")
-
 async def root():
     return "Hola, FastApi"
+
+@app.get("/url")
+async def root():
+    app = FastAPI()
+
+    # Ruta raíz que retorna un saludo
+    @app.get("/")
+    async def root():
+        return "Hola, FastApi"
+
+    # Ruta que retorna la URL de GitHub
+    @app.get("/url")
+    async def root():
+        return {"url_github": "Github.com/Sinisterra-dev"}
