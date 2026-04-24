c0da818627605c96c9c2a3badca42ddc71fb227a
Alexander Sinisterra <alexander.sinisterra.moreno@gmail.com>
2026-04-24 14:30:02 -0500

    Creación de users

--- Backend/users.py
+++ Backend/users.py
@@ -10,14 +10,33 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
+class User(BaseModel):
+    id: int
+    name: str
+    surname: str
+    url: str
+    age: int
 
-@app.get("/users")
-async def users():
+users_list = [
+    User(id=1, name="Alexander", surname="Sinisterra", url="https://sinisterradev.com", age=30),
+    User(id=2, name="Fabiola", surname="Moreno", url="https://fabiola.com", age=28),
+    User(id=3, name="Yulieth", surname="Bravo", url="https://yb.com", age=25)
+]
+# Endpoint para obtener todos los usuarios (ejemplo de datos estáticos)
+@app.get("/users_example")
+async def usersexample():
     return [{"nombre": "Alexander", "apellido": "Sinisterra", "web": "https://sinisterradev.com"},
             {"nombre": "Fabiola", "apellido": "Moreno", "web": "https://fabiola.com"},
             {"nombre": "Yulieth", "apellido": "Bravo", "web": "https://yb.com"}]
 
-            
+@app.get("/users")
+async def users():
+    return users_list   
 
-            
+@app.get("/user/{id}")
+async def user(id: int):
+    users = filter(lambda user: user.id == id, users_list)
+    return list(users)[0]
+    
+
              
